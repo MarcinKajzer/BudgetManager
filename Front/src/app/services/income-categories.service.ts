6f197c02718086927d073be1588ea2f5482b9750
Marcin Kajzer <kajzer-marcin@wp.pl>
2023-11-16 21:09:39 +0100

    Update income category service.

--- Front/src/app/services/income-categories.service.ts
+++ Front/src/app/services/income-categories.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IncomeCategory } from '../types/income-category.type';
-import { Observable, Subject, concatMap } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
@@ -11,9 +11,11 @@ export class IncomeCategoriesService {
 
   apiUrl = environment.apiUrl;
 
+  private categories: IncomeCategory[];
   private categories$: Subject<IncomeCategory[]>;
 
-   constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient) {
+    this.categories = [];
     this.categories$ = new Subject();
   }
 
@@ -22,37 +24,53 @@ export class IncomeCategoriesService {
   }
 
   refreshCategory(): void {
-    this.httpClient.get<IncomeCategory[]>(`${this.apiUrl}/IncomeCategory`, {withCredentials: true}).subscribe(categories => this.categories$.next(categories));
+    this.httpClient.get<IncomeCategory[]>(`${this.apiUrl}/IncomeCategory`, {withCredentials: true})
+      .subscribe(categories => {
+        this.categories = categories;
+        this.categories$.next(categories);
+      }
+    );   
   }
 
-  //Podejście z pobieraniem wszystkich kategorii za każdym razem (proste i powtarzalne rozwiązanie), ale nie najlepsze (doładowywać tylko to co się zmieniło)
   addCategory(categoryName: string) {
     const payload = {
       name: categoryName
     }
-    this.httpClient.post(`${this.apiUrl}/IncomeCategory`, payload, {withCredentials: true})
-      .pipe(
-        concatMap(() => this.httpClient.get<IncomeCategory[]>(`${this.apiUrl}/IncomeCategory`, {withCredentials: true}))
-      )
-      .subscribe(categories => this.categories$.next(categories))
+
+    this.httpClient.post<string>(`${this.apiUrl}/IncomeCategory`, payload, {withCredentials: true})
+      .subscribe((categoryId: string) => {
+        const category: IncomeCategory = {
+          id: categoryId,
+          name: categoryName
+        }
+
+        this.categories.push(category);
+        this.categories$.next(this.categories);
+      }
+    );
   }
 
   editCategory(id: string, name: string) {
     const payload = {
       name
     }
+
     this.httpClient.put(`${this.apiUrl}/IncomeCategory/${id}`, payload, {withCredentials: true})
-      .pipe(
-        concatMap(() => this.httpClient.get<IncomeCategory[]>(`${this.apiUrl}/IncomeCategory`, {withCredentials: true}))
-      )
-      .subscribe(categories => this.categories$.next(categories))
+      .subscribe(() => {
+        const category = this.categories.find(c => c.id == id);
+        category!.name = name;
+        this.categories$.next(this.categories);
+      }
+    )
   }
 
   deleteCategory(id: string) {
-    this.httpClient.delete(`${this.apiUrl}/IncomeCategory/${id}`)
-      .pipe(
-        concatMap(() => this.httpClient.get<IncomeCategory[]>(`${this.apiUrl}/IncomeCategory`, {withCredentials: true}))
-      )
-      .subscribe(categories => this.categories$.next(categories))
+    this.httpClient.delete(`${this.apiUrl}/IncomeCategory/${id}`, {withCredentials: true})
+      .subscribe(() => {
+        this.categories = this.categories.filter(c => c.id != id);
+        this.categories$.next(this.categories);
+      }
+    )
   }
+
 }
