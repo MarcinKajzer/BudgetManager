50ee926defe75b90e8646f0144f3599521a780a9
Marcin Kajzer <kajzer-marcin@wp.pl>
2023-11-20 17:36:50 +0100

    Fix categories redirect.

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule, Routes } from '@angular/router';
 import { IncomesComponent } from './components/incomes/incomes.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CategoriesListComponent } from './components/categories/categories-list.component';
@@ -9,48 +9,54 @@ import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
 import { IncomeCategoriesListComponent } from './components/categories/income-categories-list/income-categories-list.component';
 import { ExpenseCategoriesListComponent } from './components/categories/expense-categories-list/expenses-categories-list.component';
 
-const routes = [
+const routes: Route[] = [
   {
     path: 'signin',
+    title: 'BM - Sign In',
     component: SignInComponent
   },
   {
     path: 'signup',
+    title: 'BM - Sign Up',
     component: SignUpComponent
   },
   {
     path: 'expenses',
+    title: 'BM - Expenses',
     component: ExpensesComponent
   },
   {
     path: 'incomes',
+    title: 'BM - Incomes',
     component: IncomesComponent
   },
   {
     path: 'dashboard',
+    title: 'BM - Dashboard',
     component: DashboardComponent
   },
   {
     path: 'categories', 
     component: CategoriesListComponent,
     children: [
-      // {
-      //   path: '',
-      //   redirectTo: 'expenses',
-      //   pathMatch: 'full'
-      // },
+      {
+        path: '',
+        redirectTo: '/categories/expenses',
+        pathMatch: 'full',
+      },
       {
         path: 'incomes',
+        title: 'BM - Incomes Categories',
         component: IncomeCategoriesListComponent, 
       },
       {
         path: 'expenses',
+        title: 'BM - Expenses Categories',
         component: ExpenseCategoriesListComponent, 
-      }
+      },
     ]
   }
 ]
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
